490224965d9e5a46885a17d4041b36f674d09e7b
Lucia Serer <lserer@pav-online.com>
2019-04-26 12:25:49 +0200

    bootstrap 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // <-- Bootstrap
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent
+ ],
+ imports: [
+   BrowserModule,
+   AppRoutingModule,
+   NgbModule.forRoot()
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
